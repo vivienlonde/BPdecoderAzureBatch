e788cc16269ca58a7474ce48251c18a5a3e53402
vivienlonde <vivien.londe@gmail.com>
2021-04-23 17:56:00 +0200

    first commit

--- src/config.py
+++ src/config.py
@@ -15,13 +15,13 @@
 
 # import "config.py" in "python_quickstart_client.py "
 
-_BATCH_ACCOUNT_NAME = ''  # Your batch account name
-_BATCH_ACCOUNT_KEY = ''  # Your batch account key
-_BATCH_ACCOUNT_URL = ''  # Your batch account URL
-_STORAGE_ACCOUNT_NAME = ''  # Your storage account name
-_STORAGE_ACCOUNT_KEY = ''  # Your storage account key
+_BATCH_ACCOUNT_NAME = 'myownbatchaccount'  # Your batch account name
+_BATCH_ACCOUNT_KEY = 'KRDjqzdnN548Mo8rjfvM0gAHpWn0SqFHQLd9H/QlaQMbnF3M1FJ1TiQPYMX3mLEPw7QLYilFIaxf4UVIOlNThw=='  # Your batch account key
+_BATCH_ACCOUNT_URL = 'https://myownbatchaccount.westeurope.batch.azure.com'  # Your batch account URL
+_STORAGE_ACCOUNT_NAME = 'mystorageforbatch'  # Your storage account name
+_STORAGE_ACCOUNT_KEY = 'GrWmz7B2qt6ewPypzAVSe42DYgX7daySFEBsG+ORNHcAh+48pBwHhMks4vg21Oe66EGIsSog44zUWIf7v6ocaQ=='  # Your storage account key
 _POOL_ID = 'PythonQuickstartPool'  # Your Pool ID
 _POOL_NODE_COUNT = 2  # Pool node count
-_POOL_VM_SIZE = 'STANDARD_A1_v2'  # VM Type/Size
-_JOB_ID = 'PythonQuickstartJob'  # Job ID
+_POOL_VM_SIZE = 'Standard_D2_v3'  # VM Type/Size
+_JOB_ID = 'TestJob'  # Job ID
 _STANDARD_OUT_FILE_NAME = 'stdout.txt'  # Standard Output file
